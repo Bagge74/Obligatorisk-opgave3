4ac2fbc20ab778d6ce76b683eee531c34dd607c5
Bagge74 <94108235+Bagge74@users.noreply.github.com>
2022-03-07 21:44:51 +0100

    Add files via upload

--- opgave3.js
+++ opgave3.js
@@ -9,9 +9,13 @@ Vue.createApp({
     }, methods: {
         save(word){
             this.words.push(word)
+
             this.words.push(word.toUpperCase())
+            
             this.words.push(word.toLowerCase())
+            
             this.words.push(word.charAt(0).toUpperCase() + word.slice(1))
+            
             this.words.push(word.slice(0,word.length-1) + word.charAt(word.length-1).toUpperCase())
 
             let reversed = ""
@@ -19,13 +23,9 @@ Vue.createApp({
                 reversed += word.charAt(i)
             }
             this.words.push(reversed)
-        },
-        show(){
+        
             this.result = this.words
         },
-        clear(){
-            this.words = []
-            this.result = null
-        }
+        
     }
 }).mount ("#app")
